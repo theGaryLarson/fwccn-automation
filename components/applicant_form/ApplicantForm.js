531583ec4806c4f0ed1be22ac40858a583b76a28
theGaryLarson <gary.larson316@gmail.com>
2023-04-08 09:53:34 -0700

    WIP: Attempting to connect this to backend via api/data.js
    Plan on leaving the rest for frontend  team to work on.

--- components/applicant_form/ApplicantForm.js
+++ components/applicant_form/ApplicantForm.js
@@ -4,49 +4,120 @@ import styles from "./ApplicantForm.module.css"
 //  contains two connections. one local mysql connection and another cloud-based mongodb connection
 // TODO: ensure handleSubmit passes the correct information along and method, headers, and body is correct
 function ApplicantForm({ databaseType, database, collection }) {
-    const [result, setResult] = useState("");
+    const [result, setResult] = useState({
+        fName: "",
+        lName: "",
+        socialSecLastFour: "",
+        lastHelpDate: "",
+        householdIncome: "",
+    });
+
+    // TODO: find way to test and ensure setApplicantInfo is working correctly. (i.e. updating json fields and replacing
+    //  setResult correctly
+    function setApplicantInfo(value) {
+        setResult((prevState) => ({ ...prevState, ...value }));
+        // Update the contents of the result element
+        const resultElement = document.getElementById("result");
+        resultElement.textContent = JSON.stringify(result);
+    }
 
     async function handleSubmit(event) {
         event.preventDefault();
 
-        const filter = JSON.parse(event.target.filter.value);
-        const projection = JSON.parse(event.target.projection.value);
-
-        const response = await fetch("/api/data?dbType=" + databaseType, {
+        const response = await fetch("/api/data", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
-            body: JSON.stringify({ database, collection, filter, projection }),
+            body: JSON.stringify({
+                dbType: databaseType,
+                database,
+                collection,
+                // todo: update iteratively as fields are decided and frontend matures
+                data: {
+                    fName,
+                    lName,
+                    socialSecLastFour,
+                    lastHelpDate,
+                    householdIncome,
+                },
+            }),
         });
 
         const result = await response.json();
-        setResult(result);
+        setApplicantInfo(result);
+        setApplicantInfo(result);
+        const { fName, lName, socialSecLastFour, lastHelpDate, householdIncome } =
+            result;
     }
 
     return (
         <div>
             <form onSubmit={handleSubmit}>
                 <div className={styles.inputWrapper}>
-                    <label htmlFor="filter-input">Filter:</label>
+                    <label htmlFor="f-name-input">First Name:</label>
                     <input
                         type="text"
-                        id="filter-input"
-                        name="filter"
-                        placeholder="lorem ipsum"
+                        id="f-name-input"
+                        name="fName"
+                        placeholder="John"
+                        value=""
+                        onChange={(event) =>
+                            setApplicantInfo({fName: event.target.value})}
                         required
                     />
                 </div>
                 <div className={styles.inputWrapper}>
-                    <label htmlFor="projection-input">Projection:</label>
+                    <label htmlFor="l-name-input">Last Name:</label>
                     <input
                         type="text"
-                        id="projection-input"
-                        name="projection"
-                        placeholder="lorem ipsum"
+                        id="l-name-input"
+                        name="lName"
+                        placeholder="Doe"
+                        value=""
+                        onChange={(event) =>
+                            setApplicantInfo({lName: event.target.value})}
                         required
                     />
                 </div>
                 <div className={styles.inputWrapper}>
-                    <button type="submit">Find One</button>
+                    <label htmlFor="social-sec-input">Social Security Last Four:</label>
+                    <input
+                        type="number"
+                        id="social-sec-input"
+                        name="socialSecLastFour"
+                        placeholder="1234"
+                        value=""
+                        onChange={(event) =>
+                            setApplicantInfo({socialSecLastFour: event.target.value})}
+                        required
+                    />
+                </div>
+                <div className={styles.inputWrapper}>
+                    <label htmlFor="last-help-date-input">Last Help Date:</label>
+                    <input
+                        type="date"
+                        id="last-help-date-input"
+                        name="lastHelpDate"
+                        value=""
+                        onChange={(event) =>
+                            setApplicantInfo({lastHelpDate: event.target.value})}
+                        required
+                    />
+                </div>
+                <div className={styles.inputWrapper}>
+                    <label htmlFor="household-income-input">Household Income:</label>
+                    <input
+                        type="number"
+                        id="household-income-input"
+                        name="householdIncome"
+                        placeholder="100000"
+                        value=""
+                        onChange={(event) =>
+                            setApplicantInfo({householdIncome: event.target.value})}
+                        required
+                    />
+                </div>
+                <div className={styles.inputWrapper}>
+                    <button type="submit">Submit</button>
                 </div>
             </form>
         </div>
