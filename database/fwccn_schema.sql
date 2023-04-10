89745003974a13f35461c09f6cea8b8f906e1cf6
theGaryLarson <gary.larson316@gmail.com>
2023-04-10 10:41:51 -0700

    proof of concept schema

---
+++ database/fwccn_schema.sql
@@ -0,0 +1,32 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema fwccn_applicants
+-- -----------------------------------------------------
+DROP SCHEMA IF EXISTS `fwccn_applicants` ;
+CREATE SCHEMA IF NOT EXISTS `fwccn_applicants` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci ;
+USE `fwccn_applicants` ;
+-- -----------------------------------------------------
+-- Table `applicants`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `applicants` ;
+
+CREATE TABLE IF NOT EXISTS `applicants` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `timestamp` DATE NOT NULL,
+  `f_name` VARCHAR(45) NOT NULL,
+  `l_name` VARCHAR(45) NOT NULL,
+  `social_sec_last_four` INT(4) NOT NULL,
+  `last_help_date` DATE NULL,
+  `household_income` DECIMAL(7,2) NOT NULL,
+  PRIMARY KEY (`id`))
+ENGINE = InnoDB;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
