7137080dd232debe8d583d23bd64181d26c4f55f
theGaryLarson <gary.larson316@gmail.com>
2023-04-16 13:17:30 -0700

    independent file containing or form-data-object

---
+++ utils/form-data-object.js
@@ -0,0 +1,11 @@
+const formDataObject = {
+    timeStamp: "",
+    status: "PENDING",
+    fName: "",
+    lName: "",
+    socialSecLastFour: "",
+    lastHelpDate: "",
+    householdIncome: "",
+};
+
+export default formDataObject;
