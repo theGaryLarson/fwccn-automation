7016e58df75709342572d7a6fe1c35b769e0760a
theGaryLarson <gary.larson316@gmail.com>
2023-04-27 21:20:44 -0700

    get api call

---
+++ src/pages/api/mongoose-tutorial/get.js
@@ -0,0 +1,17 @@
+import connectMongo from "../../../../utils/connectMongo";
+import Applicant from "../../../../models/applicant_schema";
+
+/**
+ *
+ * @param {import('next').NextApiRequest} req
+ * @param {import('next').NextApiResponse} res
+ */
+export default async function getApplicant(req, res) {
+    const { id } = req.query;
+    await connectMongo();
+    console.log('CONNECTED TO MONGO');
+    console.log(`RETRIEVING APPLICANT WITH ID: ${id}`);
+    const applicant = await Applicant.findById(id);
+    console.log(`RETRIEVED APPLICANT WITH ID: ${id}`);
+    res.json(applicant);
+}
