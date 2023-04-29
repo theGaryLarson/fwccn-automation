c37ff56ec355cc032dfceacde3edff9820c12f03
theGaryLarson <gary.larson316@gmail.com>
2023-04-29 11:54:46 -0700

    placeholder components that need coded.

---
+++ components/RaceData.js
@@ -0,0 +1,2 @@
+// this might better serve as an object that is passed into components as a prop than its own component.
+// let's keep our minds open as we connect the components handleInputChange methods.
