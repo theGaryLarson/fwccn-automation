e8858817f3d09e09e270d1eefe218080bc3a259e
theGaryLarson <gary.larson316@gmail.com>
2023-05-12 22:58:38 -0700

    needs tweaking

--- components/ChildComponent.js
+++ components/ChildComponent.js
@@ -1,15 +1,16 @@
 import styles from "./applicant_form/ApplicantForm.module.css";
 
 // applicant children information
-export default function ChildrenComponent({formData, onComponentInputChange}) {
+export default function ChildComponent({formData, onComponentInputChange}) {
     const handleInputChange = (event) => {
         onComponentInputChange(event);
     };
 
     return (
-        <div>
+        <div className={`border-2 border-black p-4 box m-4`}>
+            <h1>Child Information</h1>
             <div className={styles.inputWrapper}>
-                <label htmlFor="hasChildrenUnder18">Do you have children under 18?</label>
+                <label htmlFor="hasChildrenUnder18">Do you have children under 18 living at the residence?</label>
                 <select
                     id="hasChildrenUnder18"
                     name="hasChildrenUnder18"
@@ -21,7 +22,8 @@ export default function ChildrenComponent({formData, onComponentInputChange}) {
                     <option value={false}>No</option>
                 </select>
             </div>
-            <div className={styles.inputWrapper}>
+            { formData.children.hasChildrenUnder18 === 'true' && <div>
+                <div className={styles.inputWrapper}>
                 <label htmlFor="boysCount">Number of boys</label>
                 <input
                     type="number"
@@ -31,96 +33,97 @@ export default function ChildrenComponent({formData, onComponentInputChange}) {
                     onChange={handleInputChange}
                     required
                 />
-            </div>
-            <div className={styles.inputWrapper}>
-                <label htmlFor="boysAges">Ages of boys</label>
-                <input
-                    type="text"
-                    id="boysAges"
-                    name="boysAges"
-                    value={formData.children.boysAges.join(", ")}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className={styles.inputWrapper}>
-                <label htmlFor="girlsCount">Number of girls</label>
-                <input
-                    type="number"
-                    id="girlsCount"
-                    name="girlsCount"
-                    value={formData.children.girlsCount}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className={styles.inputWrapper}>
-                <label htmlFor="girlsAges">Ages of girls</label>
-                <input
-                    type="text"
-                    id="girlsAges"
-                    name="girlsAges"
-                    value={formData.children.girlsAges.join(", ")}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className={styles.inputWrapper}>
-                <label htmlFor="nonBinaryCount">Number of non-binary children</label>
-                <input
-                    type="number"
-                    id="nonBinaryCount"
-                    name="nonBinaryCount"
-                    value={formData.children.nonBinaryCount}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className={styles.inputWrapper}>
-                <label htmlFor="nonBinaryAges">Ages of non-binary children</label>
-                <input
-                    type="text"
-                    id="nonBinaryAges"
-                    name="nonBinaryAges"
-                    value={formData.children.nonBinaryAges.join(", ")}
-                    onChange={handleInputChange}
+                </div>
+                <div className={styles.inputWrapper}>
+                    <label htmlFor="boysAges">Ages of boys</label>
+                    <input
+                        type="text"
+                        id="boysAges"
+                        name="boysAges"
+                        value={formData.children.boysAges.join(", ")}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <div className={styles.inputWrapper}>
+                    <label htmlFor="girlsCount">Number of girls</label>
+                    <input
+                        type="number"
+                        id="girlsCount"
+                        name="girlsCount"
+                        value={formData.children.girlsCount}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <div className={styles.inputWrapper}>
+                    <label htmlFor="girlsAges">Ages of girls</label>
+                    <input
+                        type="text"
+                        id="girlsAges"
+                        name="girlsAges"
+                        value={formData.children.girlsAges.join(", ")}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <div className={styles.inputWrapper}>
+                    <label htmlFor="nonBinaryCount">Number of non-binary children</label>
+                    <input
+                        type="number"
+                        id="nonBinaryCount"
+                        name="nonBinaryCount"
+                        value={formData.children.nonBinaryCount}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <div className={styles.inputWrapper}>
+                    <label htmlFor="nonBinaryAges">Ages of non-binary children</label>
+                    <input
+                        type="text"
+                        id="nonBinaryAges"
+                        name="nonBinaryAges"
+                        value={formData.children.nonBinaryAges.join(", ")}
+                        onChange={handleInputChange}
 
-                    required
-                />
-            </div>
-            <div className={styles.inputWrapper}>
-                <label htmlFor="relationshipToChildren">Relationship to children</label>
-                <input
-                    type="text"
-                    id="relationshipToChildren"
-                    name="relationshipToChildren"
-                    value={formData.children.relationshipToChildren}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className={styles.inputWrapper}>
-                <label htmlFor="schoolDistrict">School district</label>
-                <input
-                    type="text"
-                    id="schoolDistrict"
-                    name="schoolDistrict"
-                    value={formData.children.schoolDistrict}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className={styles.inputWrapper}>
-                <label htmlFor="schools">Schools attended</label>
-                <input
-                    type="text"
-                    id="schools"
-                    name="schools"
-                    value={formData.children.schools.join(", ")}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
+                        required
+                    />
+                </div>
+                <div className={styles.inputWrapper}>
+                    <label htmlFor="relationshipToChildren">Relationship to children</label>
+                    <input
+                        type="text"
+                        id="relationshipToChildren"
+                        name="relationshipToChildren"
+                        value={formData.children.relationshipToChildren}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <div className={styles.inputWrapper}>
+                    <label htmlFor="schoolDistrict">School district</label>
+                    <input
+                        type="text"
+                        id="schoolDistrict"
+                        name="schoolDistrict"
+                        value={formData.children.schoolDistrict}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+                <div className={styles.inputWrapper}>
+                    <label htmlFor="schools">Schools attended</label>
+                    <input
+                        type="text"
+                        id="schools"
+                        name="schools"
+                        value={formData.children.schools.join(", ")}
+                        onChange={handleInputChange}
+                        required
+                    />
+                </div>
+            </div>}
         </div>
     );
 }
