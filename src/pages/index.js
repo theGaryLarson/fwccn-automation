81b625e9c76d431d26ee5318f603954ec8112461
theGaryLarson <gary.larson316@gmail.com>
2023-05-12 23:00:15 -0700

    index complete

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,8 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div>
-            <h1 className="font-bold">FWCCN Applicant Form</h1>
-        </div>
+    <main className="flex min-h-screen flex-col items-center justify-between p-8">
+        <h1 className="font-bold">FWCCN Applicant Form</h1>
         <div>
             <ApplicantForm></ApplicantForm>
         </div>
