e07a1ec8d25859ff47078e614770ebffe406c56d
theGaryLarson <gary.larson316@gmail.com>
2023-05-15 22:56:52 -0700

    completed component

--- components/OtherLastNamesComponent.js
+++ components/OtherLastNamesComponent.js
@@ -1,12 +1,13 @@
 import styles from "./applicant_form/ApplicantForm.module.css";
 import {useState} from "react";
 import style from "./AdultComponent.module.css";
+import {ordinalNumber, toTitleCase} from "../lib/util";
 
 // applicant race information
 export default function OtherLastNamesComponent({formData, onComponentInputChange }) {
     const [hasOtherNames, setHasOtherNames] = useState(formData.otherNames.hasOtherNames || false)
     const [otherNames, setOtherNames] = useState(formData.otherNames.additionalNames || [])
-    const handleInputChange = (event) => {
+    const handleInputChange = (event, index) => {
         const {name, value} = event.target;
         if (name === 'hasOtherNames') {
             const booleanValue = value === 'true';
@@ -34,8 +35,26 @@ export default function OtherLastNamesComponent({formData, onComponentInputChang
             }
 
 
-        } else {
-            const updateOtherNames = formData.otherNames.additionalNames
+        } else if (index !== null) {
+            const updateOtherNames = formData.otherNames.additionalNames.map((otherName, i) => {
+                if (i === index) {
+                    return {
+                        ...otherName,
+                        [name]: value
+                    };
+                }
+                return otherName;
+            });
+
+            onComponentInputChange({
+                target: {
+                    name: 'otherNames',
+                    value: {
+                        ...formData.otherNames,
+                        additionalNames: updateOtherNames
+                    }
+                }
+            });
         }
 
     }
@@ -77,14 +96,14 @@ export default function OtherLastNamesComponent({formData, onComponentInputChang
     };
 
     return (
-        <div>
-            <div className={`${styles.inputWrapper} mt-4`}>
+        <div className={`border-2 border-gray-600 p-4 box mt-4 mb-4`}>
+            <div className={`${styles.inputWrapper} `}>
                 <label htmlFor="otherLastNames">Have you gone by any other names?</label>
                 <select
                     id="otherLastNames"
                     name="hasOtherNames"
                     value={formData.otherNames.hasOtherNames.toString()}
-                    onChange={handleInputChange}
+                    onChange={(event) => handleInputChange(event, null)}
                     required
                 >
                     <option value={'false'}>No</option>
@@ -96,20 +115,20 @@ export default function OtherLastNamesComponent({formData, onComponentInputChang
                     {formData.otherNames.additionalNames.map((otherName, index) => (
                         <div key = {index}>
                             <div className={'flex items-center'}>
-                                <h2 className={'mr-36 font-bold'}>{index}: Additional Name</h2>
+                                <h2 className={'mr-36 font-bold'}>{ordinalNumber(index)} Additional Name</h2>
                                 <button className={`${styles.removeButton}`} type={'button'} onClick={() => handleRemoveName(index)}>
                                     Remove Name
                                 </button>
                             </div>
                             <div className={styles.inputWrapper}>
-                                <label htmlFor={`otherFirstName-${index + 1}`}>Additional First Name:</label>
+                                <label htmlFor={`otherFirstName-${index}`}>First Name:</label>
                                 <input
                                     type="text"
-                                    id={`otherFirstName-${index + 1}`}
+                                    id={`otherFirstName-${index}`}
                                     name="otherFirstName"
                                     placeholder="Larry"
                                     value={otherName.otherFirstName}
-                                    onChange={handleInputChange}
+                                    onChange={(event) => handleInputChange(event, index)}
                                 />
 
                             </div>
@@ -121,7 +140,7 @@ export default function OtherLastNamesComponent({formData, onComponentInputChang
                                     name="otherMiddleInitial"
                                     placeholder="M"
                                     value={otherName.otherMiddleInitial}
-                                    onChange={handleInputChange}
+                                    onChange={(event) => handleInputChange(event, index)}
                                 />
                             </div>
                             <div className={styles.inputWrapper}>
@@ -130,15 +149,16 @@ export default function OtherLastNamesComponent({formData, onComponentInputChang
                                     type="text"
                                     id={`otherMiddleInitial-${index}`}
                                     name="otherLastName"
+                                    className={'mb-4'}
                                     placeholder="Garson"
                                     value={otherName.otherLastName}
-                                    onChange={handleInputChange}
+                                    onChange={(event) => handleInputChange(event, index)}
                                 />
                             </div>
                         </div>
                     ))}
-                    <button className={`${style.addAdultButton}`} type="button" onClick={handleAddName}>
-                        Add Adult
+                    <button className={`${style.addButton}`} type="button" onClick={handleAddName}>
+                        Add Name
                     </button>
                 </div>)}
         </div>
