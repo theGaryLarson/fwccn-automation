cc28d8ce7b5e5cae414d4a059568ac7e257dd023
Ali Sharif <alisharif29@gmail.com>
2023-05-18 01:08:28 -0700

    update schema with regex

--- models/applicant_schema.js
+++ models/applicant_schema.js
@@ -6,12 +6,15 @@ import {
 
 const applicantSchema = new Schema({
     timestamp: {
-        type: String
+        type: String,
+        match: [/^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/, 'Format must be YYYY-MM-DD HH:MM:SS'],
+        required: true
     },
     status: {
         type: String,
         enum: ['PENDING', 'APPROVED', 'DENIED', 'APPROVED-OVERRIDE'],
         default: 'PENDING',
+        required: true
     },
     interviewer: {
         type: String,
@@ -23,14 +26,15 @@ const applicantSchema = new Schema({
     },
     helpRequested: {
         type: String,
-        enum: ['rent', 'gasoline', 'busTicket', 'food'],
-        default: '',
+        enum: ['rent', 'gasoline', 'busTicket'],
+        required: true
     },
     licensePlate: {
         type: String
     },
     licensePlateState: {
-        type: String
+        type: String,
+        match: [/[A-Z]{2}/, 'Must enter 2-letter abbreviation for the state']
     },
     reasonForNeed: {
         type: String,
@@ -57,7 +61,10 @@ const applicantSchema = new Schema({
         required: true
     },
     otherNames: {
-        hasOtherNames: Boolean,
+        hasOtherNames: {
+            type: Boolean,
+            required: true
+        },
         additionalNames:[{
             otherFirstName: {
                 type: String
@@ -70,10 +77,7 @@ const applicantSchema = new Schema({
             }
         }],
     },
-    gender: {
-        type: String
-    },
-    age: {
+    applicantAge: {
         type: Number,
         min: [18, "You must be 18 years or older to apply"],
         validate: { // custom validator
@@ -84,19 +88,23 @@ const applicantSchema = new Schema({
         } // end validate
     },
     phone: {
-        type: Number
+        type: Number,
+        match: [/^[0-9]{10}/, 'Enter 10 digit phone number exclude any additional characters']
     },
     income: {
         currentMonthlyIncome: {
             type: Number,
+            match: [/^\d*[1-9]\d*$/, 'Must enter a positive number'],
             required: true
         },
         monthlyIncomeLast12Months: {
             type: Number,
+            match: [/^\d*[1-9]\d*$/, 'Must enter a positive number'],
             required: true
         },
         totalHouseholdMembersIncomeSupports: {
             type: Number,
+            match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             required: true
         },
     },
@@ -115,10 +123,11 @@ const applicantSchema = new Schema({
         },
         idStateIssued: {
             type: String,
-            minlength: [2, 'Enter 2 letter state abbreviation']
+            match: /[A-Z]{2}/
         },
         socialSecLastFour: {
             type: Number,
+            match: [/[0-9]{4}/, 'Enter only the last 4 digits of applicant\'s social security number'],
             required: true
         },
     },
@@ -130,9 +139,11 @@ const applicantSchema = new Schema({
         },
         durationXpHomelessness: {
             type: Number,
+            match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number']
         },
         whyHomeless: {
             type: String,
+            required: true
         },
         tempAddress: {
             street1: {
@@ -146,105 +157,141 @@ const applicantSchema = new Schema({
             },
             state: {
                 type: String,
+                match: [/[A-Z]{2}/, 'Enter 2-letter abbrevation for state'],
             },
             zip: {
                 type: String,
+                match: [/^\d{5}(?:-\d{4})?$/, 'Enter zip code in the following format ##### or #####-####']
             },
         },
         children: {
             hasChildrenUnder18: {
-                type: Boolean
+                type: Boolean,
+                required: true
             },
-            kids: [
-                {
-                    gender: {
-                        type: String,
-                        required: true
-                    },
-                    age: {
-                        type: Number,
-                        required: true
-                    },
-                    school: {
-                        type: String,
-                        required: true
-                    },
-                    schoolDistrict: {
-                        type: String,
-                        required: true
-                    },
-                    relationshipToApplicant: {
-                        type: String,
-                        required: true
+            kids: {
+                type: [
+                    {
+                        gender: {
+                            type: String,
+                            required: function() {
+                                return this.children.hasChildrenUnder18 === true;
+                            }
+                        },
+                        age: {
+                            type: Number,
+                            required: function() {
+                                return this.children.hasChildrenUnder18 === true;
+                            }
+                        },
+                        school: {
+                            type: String
+                        },
+                        schoolDistrict: {
+                            type: String
+                        },
+                        relationshipToApplicant: {
+                            type: String,
+                            required: function() {
+                                return this.children.hasChildrenUnder18 === true;
+                            }
+                        }
                     }
-                }
-            ],
-            boysCount: {
-                type: Number
+                ]
             },
-             boysAges: [{
-                type: Number
-             }],
-             girlsCount: {
-                type: Number
-             },
-             girlsAges: [{
-                type: Number
-             }],
-             relationToApplicant: [{
-                    type: String
-             }],
-             schoolDistrict: [{
+            boysCount: {
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
+
+            },
+            boysAges: [{
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
+            }],
+            girlsCount: {
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
+            },
+            girlsAges: [{
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
+            }],
+            relationToApplicant: [{
                 type: String
-             }],
-             schools: [{
+            }],
+            schoolDistrict: [{
                 type: String
-             }],
+            }],
+            schools: [{
+                type: String
+            }],
         },
         otherAdults: [{
             isOtherAdultsAtResidence: {
                 type: Boolean,
                 required: true
             },
-            adults: [
-                {
-                    adultFName: {
-                        type: String
-                    },
-                    adultMiddleInitial: {
-                        type: String
-                    },
-                    adultLName: {
-                        type: String
-                    },
-                    adultGender: {
-                        type: String
-                    },
-                    adultAge: {
-                        type: Number
-                    },
-                    relationshipToAdult: {
-                        type: String
+            adults: {
+                type: [
+                    {
+                        adultFName: {
+                            type: String,
+                            required: function () {
+                                return this.isOtherAdultsAtResidence === true;
+                            }
+                        },
+                        adultMiddleInitial: {
+                            type: String
+                        },
+                        adultLName: {
+                            type: String,
+                            required: function () {
+                                return this.isOtherAdultsAtResidence === true;
+                            }
+                        },
+                        adultGender: {
+                            type: String,
+                            required: function () {
+                                return this.isOtherAdultsAtResidence === true;
+                            }
+                        },
+                        adultAge: {
+                            type: Number,
+                            required: function () {
+                                return this.isOtherAdultsAtResidence === true;
+                            }
+                        },
+                        relationshipToAdult: {
+                            type: String,
+                            required: function () {
+                                return this.isOtherAdultsAtResidence === true;
+                            }
+                        }
                     }
-                }
-            ]
+                ]
+            }
 
         }],
         homeAddress: {
             homeStreet1: {
-                type: String
+                type: String,
+                required: true
             },
             homeStreet2: {
                 type: String
             },
             homeCity: {
-                type: String
+                type: String,
+                required: true
             },
             homeState: {
-                type: String
+                type: String,
+                match: [/[A-Z]{2}/, 'Enter 2-letter abbreviation for state'],
+                required: true
             },
             homeZip: {
-                type: Number
+                type: Number,
+                match: [/^\d{5}(?:-\d{4})?$/, 'Enter zip code in the following format ##### or #####-####']
             },
         },
         landLord: {
@@ -252,7 +299,8 @@ const applicantSchema = new Schema({
                 type: String
             },
             landLordPhone: {
-                type: String
+                type: String,
+                match: [/^[0-9]{10}/, 'Enter 10 digit phone number. Exclue any additional characters'],
             },
             verified: {
                 type: Boolean
@@ -268,51 +316,65 @@ const applicantSchema = new Schema({
                     type: String
                 },
                 landLordZip: {
-                    type: Number
+                    type: Number,
+                    match: [/^\d{5}(?:-\d{4})?$/, 'Enter zip code in the following format ##### or #####-####']
                 },
             },
         },
         houseHoldIncome: {
             totalHouseholdIncome: {
-                type: Number
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             },
             totalSupportMembers: {
-                type: Number
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             },
             singleMaleHeadOfHousehold: {
-                type: Boolean
+                type: Boolean,
+                required: true
             },
             singleFemaleHeadOfHousehold: {
-                type: Boolean
+                type: Boolean,
+                required: true
             },
         },
         race: {
             americanIndianOrAlaskaNative: {
-                type: Number
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             },
             whiteOrCaucasian: {
-                type: Number
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             },
             asianAsianAmerican: {
-                type: Number
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             },
             otherRace: {
-                type: Number
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             },
             blackAfricanAmerican: {
-                type: Number
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             },
             multiRacial: {
-                type: Number
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             },
             latinoAmericanHispanic: {
-                type: Number
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             },
             unknown: {
-                type: Number
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             },
             nativeAmericanPacificIslander: {
-                type: Number
+                type: Number,
+                match: [/^\d*[0-9]\d*$/, 'Must enter a non-negative number'],
             },
         },
     },
