8784e2ccc03fb471af2dbec0bec6d356e381c141
theGaryLarson <gary.larson316@gmail.com>
2023-05-27 18:59:09 -0700

    removed verification checkbox from here to HomeAddressComponent

--- components/LandlordComponent.js
+++ components/LandlordComponent.js
@@ -6,26 +6,11 @@ export default function LandLordComponent({ formData, onComponentInputChange }) 
     const handleInputChange = (event) => {
         onComponentInputChange(event)
     }
-    const [isVerified, setIsVerified] = useState(false);
 
-    const handleCheckboxChange = () => {
-        setIsVerified(!isVerified);
-        const updatedLandlord = {
-            ...formData.landLord,
-            verified: !isVerified,
-        }
-        onComponentInputChange({
-            target: {
-                name: 'landLord',
-                value: updatedLandlord
-            }
-
-        })
-    };
     return (
         <div>
             <h1>Landlord Information</h1>
-            <div className={styles.inputWrapper}>
+            <div className={styles.componentWrapper}>
                 <label htmlFor="landlord-full-name">Landlord Full Name</label>
                 <input
                     type="text"
@@ -37,7 +22,7 @@ export default function LandLordComponent({ formData, onComponentInputChange }) 
                     required
                 />
             </div>
-            <div className={styles.inputWrapper}>
+            <div className={styles.componentWrapper}>
                 <label htmlFor="landlord-phone">Landlord Phone Number</label>
                 <input
                     type="tel"
@@ -51,66 +36,52 @@ export default function LandLordComponent({ formData, onComponentInputChange }) 
                     required
                 />
             </div>
-            <div className={styles.inputWrapper}>
+            <div className={styles.componentWrapper}>
                 <label htmlFor="landlord-street1">Landlord Street Address Line 1</label>
                 <input
                     type="text"
                     id="landlord-street1"
                     name="landLordStreet1"
                     placeholder="123 Main St"
-                    value={formData.landLord.address.landLordStreet1}
+                    value={formData.landLord.landLordAddress.landLordStreet1}
                     onChange={handleInputChange}
                     required
                 />
             </div>
-            <div className={styles.inputWrapper}>
+            <div className={styles.componentWrapper}>
                 <label htmlFor="landlord-street2">Landlord Street Address Line 2</label>
                 <input
                     type="text"
                     id="landlord-street2"
                     name="landLordStreet2"
                     placeholder="Apt. 100"
-                    value={formData.landLord.address.landLordStreet2}
+                    value={formData.landLord.landLordAddress.landLordStreet2}
                     onChange={handleInputChange}
                 />
             </div>
-            <div className={styles.inputWrapper}>
+            <div className={styles.componentWrapper}>
                 <label htmlFor="landlord-city">Landlord City</label>
                 <input
                     type="text"
                     id="landlord-city"
                     name="landLordCity"
                     placeholder="Seattle"
-                    value={formData.landLord.address.landLordCity}
+                    value={formData.landLord.landLordAddress.landLordCity}
                     onChange={handleInputChange}
                     required
                 />
             </div>
-            <div className={styles.inputWrapper}>
+            <div className={styles.componentWrapper}>
                 <label htmlFor="landlord-zip">Landlord Zip Code</label>
                 <input
                     type="number"
                     id="landlord-zip"
                     name="landLordZip"
                     placeholder="98101"
-                    value={formData.landLord.address.landLordZip}
+                    value={formData.landLord.landLordAddress.landLordZip}
                     onChange={handleInputChange}
                     required
                 />
-            </div>
-            <div className={styles.inputWrapper}>
-                <input
-                    type="checkbox"
-                    id="landLordIsVerified"
-                    className="hidden"
-                    onChange={handleCheckboxChange}
-                />
-                <label htmlFor="landLordIsVerified" className="flex items-center mt-4 select-none">
-                <span className={` ${!isVerified ? 'bg-green-500' : ''} mr-2 border rounded border-gray-400 bg-white w-5 h-5 flex items-center justify-center `}>
-                    {isVerified && '✓'}
-                </span>
-                    <span className='font-bold'>Lease Verified</span>
-                </label>
             </div>
         </div>
     );
