c6fc4688d8927caa359e4f7630a27c881605a967
theGaryLarson <gary.larson316@gmail.com>
2023-05-27 18:56:42 -0700

    some comments on thinking out requirements

--- src/pages/api/get-address.js
+++ src/pages/api/get-address.js
@@ -3,4 +3,17 @@
  */
 import Applicant from "../../../models/applicant_schema";
 
-Applicant.find()
+Applicant.find({
+    homeAddress: {
+        homeStreet1: '123 Oak St.',
+        lubbawubbadubdub: true
+    }
+})
+
+
+// get collection of all documents/records
+// iterate through and find address equal to submission address
+// display these address in a new component with the following features
+//  list them as cards
+// clicking the card will expand to show relevant information to cross check for requirements
+// needed info:  address, adults living at that address at that time.
