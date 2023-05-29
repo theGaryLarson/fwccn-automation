cef733a587b8165a80ad27dae1cf0eae8aa6a16e
theGaryLarson <gary.larson316@gmail.com>
2023-05-29 08:12:43 -0700

    WIP: added EditReviewComponent

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,15 @@
 import ApplicantForm from "../../components/applicant_form/ApplicantForm";
+import EditReviewComponent from "../../components/EditReviewComponent";
+
 
 export default function Home() {
-  return (
+    return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8">
         <h1 className="font-bold">FWCCN Applicant Form</h1>
         <div>
+            <div>
+                <EditReviewComponent/>
+            </div>
             <ApplicantForm></ApplicantForm>
         </div>
     </main>
