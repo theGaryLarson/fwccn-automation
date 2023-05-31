ec10e339c6b5550bf9c7d88580ff9e230e857d76
theGaryLarson <gary.larson316@gmail.com>
2023-05-31 01:00:19 -0700

    ADDED DATA PASS THROUGH

--- components/DecisionDetailsComponent.js
+++ components/DecisionDetailsComponent.js
@@ -1,12 +1,16 @@
 import RequirementCheckComponent from "./RequirementCheckComponent";
 import NeedSummaryComponent from "./NeedSummaryComponent";
+import {useState} from "react";
 
-export default function DecisionDetailsComponent({requirementFlags, records}) {
-
+export default function DecisionDetailsComponent(props) {
+    const { queryObject, records} = props
+    const [ focusedItem, setFocusedItem ] = useState({})
+    console.log("TYPEOF IN ROOT COMPONENT: ", setFocusedItem)
     return (
-        <div>
-            <NeedSummaryComponent/>
-            <RequirementCheckComponent/>
-        </div>
+            <div>
+                <NeedSummaryComponent focusedItem={focusedItem}/>
+                <RequirementCheckComponent queryObject={queryObject} setFocusedItem={setFocusedItem} />
+            </div>
+
     )
 }
