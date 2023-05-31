2b7184fb397eb9f4b87bc59a8be08e417436ac00
theGaryLarson <gary.larson316@gmail.com>
2023-05-31 01:05:16 -0700

    todo research: server config uncommenting may cause error.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,21 @@
 import ApplicantForm from "../../components/applicant_form/ApplicantForm";
 import EditReviewComponent from "../../components/EditReviewComponent";
 import {useState} from "react";
-
+// import getConfig from "next/config"
 
 export default function Home() {
+    // const {serverRuntimeConfig} = getConfig()
+    // console.log(serverRuntimeConfig.ipAddress)
     const [records, setRecords] = useState([]);
     return (
     <main className="flex flex-col items-center justify-between p-8">
         <h1 className="font-bold">FWCCN Applicant Form</h1>
         <div>
             <div>
+
                 <EditReviewComponent records={records} setRecords={setRecords}/>
             </div>
-            {/*<ApplicantForm></ApplicantForm>*/}
+            <ApplicantForm></ApplicantForm>
         </div>
     </main>
   )
