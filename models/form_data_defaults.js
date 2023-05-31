c663a4f0e86daff7d3a9e421a6b882d2daf26a86
theGaryLarson <gary.larson316@gmail.com>
2023-05-31 01:04:09 -0700

    removed income object (using totalHouseHoldIncome)

--- models/form_data_defaults.js
+++ models/form_data_defaults.js
@@ -26,11 +26,6 @@ const form_data_defaults = {
     applicantGender: 'Male',
     applicantAge: '33',
     phone: '9876543210',
-    // income: {
-    //     currentMonthlyIncome: 850,
-    //     monthlyIncomeLast12Months: 16750,
-    //     totalHouseholdMembersIncomeSupports: 3,
-    // },
     disabled: false,
     idSource: {
         driverLicenseOrId: 'FGSLTWHGHOBS316',
