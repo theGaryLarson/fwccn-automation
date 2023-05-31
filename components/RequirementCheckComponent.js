5eb23158aebb0a834992ce4ea170cbeb8158b403
theGaryLarson <gary.larson316@gmail.com>
2023-05-31 01:07:56 -0700

    CONNECTS TO MONGO AND PERFORMS REQUIREMENT LOGIC ON THE BACKEND

--- components/RequirementCheckComponent.js
+++ components/RequirementCheckComponent.js
@@ -1,8 +1,56 @@
-export default function RequirementCheckComponent() {
+import { useEffect, useState } from "react";
+import Accordion from "./Accordion";
+export default function RequirementCheckComponent(props) {
+    const [data, setData] = useState({});
+    const { setFocusedItem, queryObject  } = props;
+    console.log("typeOf in ReqChkComp", typeof setFocusedItem)
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await fetch(`api/requirementsCheck`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(queryObject),
+            });
+            const res = await response.json();
+            setData(res);
+        };
+        fetchData();
+    }, [queryObject]);
 
+    if (!data) {
+        return <div>Loading...</div>;
+    }
+
+    const ids = data.stateIdDuplicates ? Object.values(data.stateIdDuplicates.records) : [];
+    const addresses = data.addressDuplicates ? Object.values(data.addressDuplicates.records) : [];
     return (
         <div>
-        todo: handle requirement checks here and display data
+            <h1 className={"flex items-center justify-center font-bold"}>
+                {ids.length > 0 && 'Applicants Submission History'}
+                {addresses.length > 0 && 'Address Submission History'}
+                {ids.length < 1 && addresses.length < 1 && 'No Previous Entries'}
+            </h1>
+            {ids && (
+                <div>
+
+                    {ids.map((item, index) => (
+                        <Accordion setItemFocus={setFocusedItem} key={index} item={item} />
+                    ))}
+                </div>
+            )}
+
+
+            {addresses && (
+                <div className={"w-full"}>
+                    <div className={"place-items-center"}>
+                    </div>
+                    {addresses.map((item, index) => (
+                        <Accordion setItemFocus={setFocusedItem} key={index} item={item} />
+                    ))}
+                </div>
+            )}
         </div>
-    )
+    );
 }
