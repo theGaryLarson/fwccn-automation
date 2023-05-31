ecd0a2ec3ba2a05b43859c3803b4ecfb6959645e
theGaryLarson <gary.larson316@gmail.com>
2023-05-31 01:06:12 -0700

    WARNING: UNCOMMENTING MAY CAUSE ERRORS TODO RESEARCH ON SERVER SETUP

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  // serverRuntimeConfig: {
+  //   // Define your server configuration options here
+  //   // For example:
+  //   ipAddress: process.env.IP_ADDRESS || 'localhost',
+  //   mongodbUri: process.env.LOCAL_URI,
+  // },
 }
 
 module.exports = nextConfig
