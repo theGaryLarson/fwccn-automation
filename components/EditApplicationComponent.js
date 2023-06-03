4d5e1f7153d27a13341c1cd0a68e9ad57fa2c18e
theGaryLarson <gary.larson316@gmail.com>
2023-06-03 16:38:23 -0700

    set background to discern that form is being used to update data.

--- components/EditApplicationComponent.js
+++ components/EditApplicationComponent.js
@@ -2,12 +2,12 @@ import {useState} from "react";
 import ApplicantForm from "./applicant_form/ApplicantForm";
 
 
-export function EditApplicationComponent(props) {
+export default function EditApplicationComponent(props) {
     const {item} = props
 
 
     return (
-        <div>
+        <div className={'bg-amber-500 m-8 p-8 rounded border-2 border-indigo-700'}>
             <ApplicantForm item={ item }/>
         </div>
     )
