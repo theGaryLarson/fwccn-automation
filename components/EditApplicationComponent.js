9f59bc0c8acf69bd6c58eb1f882a436c2f9a05bb
theGaryLarson <gary.larson316@gmail.com>
2023-06-03 15:51:50 -0700

    OOPS: forgot export statement as well

--- components/EditApplicationComponent.js
+++ components/EditApplicationComponent.js
@@ -2,7 +2,7 @@ import {useState} from "react";
 import ApplicantForm from "./applicant_form/ApplicantForm";
 
 
-function EditApplicationComponent(props) {
+export function EditApplicationComponent(props) {
     const {item} = props
 
 
