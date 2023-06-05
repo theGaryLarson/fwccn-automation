dc77fb37dcbceac08842edd82fd5b40008196e86
theGaryLarson <gary.larson316@gmail.com>
2023-06-05 11:05:03 -0700

    Cleaned Code

--- components/EditApplicationComponent.js
+++ components/EditApplicationComponent.js
@@ -5,8 +5,6 @@ export default function EditApplicationComponent(props) {
     const {item, onUpdate, updateApplicant} = props
     const MemorizedApplicantForm = memo(ApplicantForm);
 
-    /*className={'mb-16  h-full bg-white flex flex-col items-stretch'*/
-
     return (
         <div className={'flex flex-col items-left w-full bg-amber-500 m-2 ml-0 p-2 rounded border-2 border-indigo-700 ' }>
             {/*<div>Item _id {item._id}</div>*/}
