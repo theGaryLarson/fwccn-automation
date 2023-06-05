37ebdf9b411c6ca61b225cf0db277003da3444b7
theGaryLarson <gary.larson316@gmail.com>
2023-06-04 23:56:02 -0700

    COMPLETE: Style & Layout

--- components/NeedSummaryComponent.js
+++ components/NeedSummaryComponent.js
@@ -3,8 +3,8 @@ import {isEmptyObject} from "../lib/util";
 
 export default function NeedSummaryComponent(props) {
     const { firstItem, focusedItem } = props;
-    const textAreaRef = useRef(focusedItem.reasonForNeed)
-    // const [status, setStatus] = useState("PENDING")
+    const reasonForNeedRef = useRef(null);
+    const futurePlansRef = useRef(null);
     const [text, setText] = useState(focusedItem.reasonForNeed || "")
     const [ isTextAreaCollapsed, setIsTextAreaCollapsed] = useState(false)
     const [ hasChildren, setHasChildren] = useState( false);
@@ -19,28 +19,25 @@ export default function NeedSummaryComponent(props) {
         }
     },[setText, setHasChildren, text, firstItem, focusedItem,] )
 
-    // function isEditable() {
-    //     const isMostRecentApplicationSubmitted = firstItem === focusedItem
-    //     const applicationPending = focusedItem.status === "PENDING"
-    //     return isMostRecentApplicationSubmitted || applicationPending
-    // }
     function handleStatusChange(event) {
         const {value} = event.target
         focusedItem.status = value
         // setStatus(value)
     }
-    const toggleTextAreaSize = () => {
+    const toggleTextAreaSize = (textAreaRef) => {
         setIsTextAreaCollapsed(!isTextAreaCollapsed);
         const textarea = textAreaRef.current;
 
         if (isTextAreaCollapsed) {
             textarea.style.height = "150px";
             textarea.style.height = `${textarea.scrollHeight}px`;
-
         } else {
             textarea.style.height = "150px";
+
+            const textareaOffset = textarea.getBoundingClientRect().top + window.scrollY - 40;
+
             window.scrollTo({
-                top: parent.top.screenTop,
+                top: textareaOffset,
                 behavior: "smooth",
             });
         }
@@ -51,67 +48,11 @@ export default function NeedSummaryComponent(props) {
             { focusedItem &&
                 (
                     <div>
-                        <div className={`flex flex-row w-full bg-gray-100 p-2 `}>
-                            <div className="w-full left">
-                                <div className={`flex flex-col left pl-4`}>
-                                    <h1><span className={"font-bold"}>Applicant:</span> {focusedItem.fName} {focusedItem.lName}</h1>
-                                </div>
-                            </div>
-                            <div className={"w-full flex justify-end"}>
-                                <div className={` w-full flex justify-end`}>
-                                    <label htmlFor={"status-edit"} className={"font-bold"}>Status:</label>
-                                    <select
-                                        id={"status-edit"}
-                                        name={"status-edit"}
-                                        value = {focusedItem.status}
-                                        onChange={handleStatusChange}
-                                        className={`text-center `}
-                                    >
-                                        <option value={"PENDING"}>PENDING</option>
-                                        <option value={"APPROVED"}>APPROVED</option>
-                                        <option value={"APPROVED-OVERRIDE"}>OVERRIDE-APPROVE</option>
-                                        <option value={"DENIED"}>DENY</option>
-                                        <option value={"NO-RETURN"}>NO-RETURN</option>
-                                    </select>
+                        <div className={`flex flex-col w-full `}>
 
-                                </div>
-                            </div>
-                        </div>
-                        <div className={`flex flex-row w-full bg-gray-100`}>
-                            <div className={' w-full left '}>
-                                <span className={"font-medium"}>Has Disability: </span>{focusedItem.disabled ? 'Yes' : 'No'}
-                            </div>
-                            <div className={`flex flex-col w-full justify-end pr-8`}>
-                                <div className={'w-full'}>
-                                    <span className={"font-medium"}>Interviewed by: </span> {focusedItem.interviewer}
-                                </div>
-                                <div className={'w-full'}>
-                                    <span className={"font-medium"}>Referred by: </span> {focusedItem?.referredBy??''}
-                                </div>
-                            </div>
-                        </div>
-                        <div className={`flex flex-col w-full bg-gray-100 p-2`}>
-                            <div className={"w-full"}>
-                                <h2 className="font-bold text-left">{hasAdults ? 'Adult Relationships:' : 'No Other Adults'}</h2>
-                                <ul className=" w-full ">
-                                    { hasAdults && focusedItem.otherAdults.adults?.map((adult, index) => {
-                                        return (
-                                            <li className=" pl-4 w-full text-left" key={index}>
-                                                <span className="font-medium">Relation:</span>
-                                                {
-                                                    adult.relationshipToAdult.toLowerCase() === 'relative' ?
-                                                        <span> {adult.relationDetails} <span className="font-medium">Age:</span> {adult.adultAge}</span>
-                                                        :
-                                                        <span> {adult.relationshipToAdult} <span className="font-medium">Age:</span> {adult.adultAge}</span>
-                                                }
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                            </div>
                             <div className={"w-full"}>
                                 {/* List of children relationships */}
-                                <h2 className="font-bold text-left">{hasChildren ? 'Child Relationships:' : 'No Children'}</h2>
+                                <h2 className="w-full m-0 font-bold bg-gray-700 text-white mt-2 mb-2 text-center">{hasChildren ? 'Child Relationships:' : 'No Children'}</h2>
                                 <ul className="w-full">
                                     {hasChildren && focusedItem.children.relationsToApplicant?.map((relationship, index) => (
                                         <li className="pl-4 w-full text-left" key={index}>
@@ -124,16 +65,46 @@ export default function NeedSummaryComponent(props) {
                                     ))}
                                 </ul>
                             </div>
+                            <div className={"w-full"}>
+                                <h2 className="w-full m-0 font-bold bg-gray-700 text-white mt-2 mb-2 text-center">{hasAdults ? 'Adult Relationships:' : 'No Other Adults'}</h2>
+                                <ul className=" w-full ">
+                                    { hasAdults && focusedItem.otherAdults.adults?.map((adult, index) => {
+                                        return (
+                                            <li className="pl-4 w-full text-left" key={index}>
+                                                <span className="font-medium">Relation:</span>
+                                                {
+                                                    adult.relationshipToAdult.toLowerCase() === 'relative' ?
+                                                        <span> {adult.relationDetails} <span className="font-medium">Age:</span> {adult.adultAge}</span>
+                                                        :
+                                                        <span> {adult.relationshipToAdult} <span className="font-medium">Age:</span> {adult.adultAge}</span>
+                                                }
+                                            </li>
+                                        )
+                                    })}
+                                </ul>
+                            </div>
                         </div>
                         <div className={"flex flex-col w-full items-center"}>
-                            <p className={"font-bold"}>Reason for Need</p>
+                            <p className="font-bold bg-gray-700 text-white mt-2 mb-2 text-center w-full" >Need Summary</p>
                             <textarea
                                 id="reasonForNeed"
                                 name="reasonForNeed"
                                 value={focusedItem.reasonForNeed}
-                                className="h-150 bg-gray-200 w-full p-4 pt-1 border-b-black  resize-none"
-                                onClick={toggleTextAreaSize}
-                                ref={textAreaRef}
+                                className="h-[150px] bg-blue-50 border-indigo-700 w-full p-4 pt-1  resize-none"
+                                onClick={() => toggleTextAreaSize(reasonForNeedRef)}
+                                ref={reasonForNeedRef}
+                                readOnly
+                            />
+                        </div>
+                        <div className={"flex flex-col w-full items-center"}>
+                            <p className="font-bold bg-gray-700 text-white mb-2 text-center w-full">Future Plans</p>
+                            <textarea
+                                id="future-plans"
+                                name="future-plans"
+                                value={focusedItem.futurePlans}
+                                className="h-[150px] bg-blue-50 border-indigo-700 w-full p-4 pt-1  resize-none"
+                                onClick={() => toggleTextAreaSize(futurePlansRef)}
+                                ref={futurePlansRef}
                                 readOnly
                             />
                         </div>
