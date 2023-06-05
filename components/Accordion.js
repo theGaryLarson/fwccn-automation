10999a73ba5c7c916b42bcbd09b94344b2ab6b1d
theGaryLarson <gary.larson316@gmail.com>
2023-06-04 23:53:23 -0700

    Style Update: Layout finalized

--- components/Accordion.js
+++ components/Accordion.js
@@ -56,55 +56,132 @@ import EditApplicationComponent from "./EditApplicationComponent";
                     <button
                         type="button"
                         onClick={toggleAccordion}
-                        className={` rounded m-1 mb-0 p-2 w-full text-center ${item.status === 'APPROVED' || item.status === 'APPROVED-OVERRIDE' ? ' bg-green-100 border-green-500 border-2' : ' bg-red-100 border-red-500 border-2'}`}
+                        className={` rounded m-1 mb-0 p-2 w-full text-center ${item.status === 'APPROVED' || item.status === 'APPROVED-OVERRIDE' ? ' bg-green-300 border-black border-2' :
+                            (item.status === "PENDING" ? 'bg-gray-300 border-black border-2' : (item.status === 'NO-RETURN' ? ' bg-orange-300 border-black border-2' : ' bg-red-300 border-black border-2'))}`}
                     >
                          <span className="font-bold">
-                            {formatNextEligibleDate(item.timestamp, 0)}
+                            {dateFormatNYears(item.timestamp, 0)}
                          </span>
                     </button>
                     {isOpen && (
                         <div className={'w-full'}>
-                            <NeedSummaryComponent focusedItem={item} firstItem={firstItem} />
-                            <div className={`flex flex-row w-full bg-gray-100 p-2 `}>
-                                <div className="bg-gray-100 rounded  w-full left">
-                                    <p className="ml-4"><span
-                                        className={"font-bold"}>Name:</span> {`${item.fName} ${item.lName}`}</p>
-                                    <p className="ml-4"><span
-                                        className={"font-bold"}>State ID:</span> {item.idSource.driverLicenseOrId}</p>
-                                    <p className="ml-4 mb-4"><span
-                                        className={"font-bold"}>SSN Last-Four:</span> {`${item.idSource.socialSecLastFour}`}
-                                    </p>
-                                    <p>
-                                        <span className={"backdrop-blur-2xl ml-4 font-bold"}>Address Verified:</span>
-                                        {item.homeAddress.verified ? 'Yes' : 'No'}
-                                    </p>
-                                    <div
-                                        className={'leading-tight bg-gray-700 w-auto pl-2 pt-2 pb-2 rounded text-white'}>
+                            <div className={'w-full bg-blue-200 mt-4 p-4 '}>
+                                <p className="text-center text-xl">{`${item.fName} ${item.lName}`} </p>
+                                <p className={"font-bold italic text-center"}>Applicant</p>
+                            </div>
+                            <div>
+                                <div>
+                                    <div className="flex justify-end space-x-4 mb-4 mt-4">
+                                        {!showForm && (
+                                            <button
+                                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                                            type="button"
+                                            onClick={async () => {
+                                                await updateApplicant(item).then(r => {
+                                                        console.log("apiResponseMesg: ", r.record)
+                                                        if (updateItem) { // New addition here
+                                                            updateItem(r.record);
+                                                        }
+                                                });
+                                            }}
+                                        >
+                                            UPDATE FORM
+                                        </button>)}
+                                        <button onClick={toggleShowForm}
+                                                className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
+                                                type="button"
+                                        >
+                                            {showForm ? `HIDE FORM` : `SHOW FORM`}
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="grid grid-cols-2 grid-rows-2 gap-4">
+                                <div className="flex flex-col items-start">
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p className=""><span className={"font-bold"}>Help Requested:</span> {item.helpRequested}</p>
+                                    </div>
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p className=""><span className={"font-bold"}>State ID:</span> {item.idSource.driverLicenseOrId}</p>
+                                    </div>
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p className=""><span className={"font-bold"}>SSN Last Four:</span> {item.idSource.socialSecLastFour}</p>
+                                    </div>
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p className=""><span className={"font-bold"}>Has Disability:</span> {item.disabled ? 'Yes' : 'No'}</p>
+                                    </div>
+                                </div>
+                                <div className="flex flex-col items-start">
+                                    <div className="whitespace-nowrap flex-1">
+                                        <label htmlFor={"status-edit"} className={"font-bold "}>Status:</label>
+                                        <select
+                                            id={"status-edit"}
+                                            name={"status-edit"}
+                                            value = {item.status}
+                                            onChange={handleStatusChange}
+                                            className={`text-center w-full ml-4`}
+                                        >
+                                            <option value={"PENDING"}>PENDING</option>
+                                            <option value={"APPROVED"}>APPROVED</option>
+                                            <option value={"APPROVED-OVERRIDE"}>OVERRIDE-APPROVE</option>
+                                            <option value={"DENIED"}>DENY</option>
+                                            <option value={"NO-RETURN"}>NO-RETURN</option>
+                                        </select>
+                                    </div>
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p className=""><span className={"font-bold"}>Interviewed By:</span> {item.interviewer}</p>
+                                    </div>
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p className=""><span className={"font-bold"}>Referred By:</span> {item.referredBy}</p>
+                                    </div>
+                                    <div className="whitespace-nowrap flex-1"></div>
+                                </div>
+                                <div className="flex flex-col items-start">
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p className=""><span className={"font-bold"}>Experiencing Homelessness:</span> {item.isHomeless ? 'Yes' : 'No'}</p>
+                                    </div>
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p className={"font-bold"}>Address:</p>
+                                    </div>
+                                    <div className="whitespace-nowrap w-full flex-1 leading-tight px-2 pt-2 pb-2 rounded border-2 bg-blue-100 border-indigo-700">
                                         <p className="ml-2">{item.homeAddress.homeStreet1}</p>
                                         <p className="ml-2">{item.homeAddress.homeStreet2}</p>
                                         <p className="ml-2">{item.homeAddress.homeCity + " " + item.homeAddress.homeState + ", " + item.homeAddress.homeZip}</p>
                                     </div>
-
                                 </div>
-                                <div className={"w-full right"}>
-                                    <p className="ml-4"><span
-                                        className={"font-bold"}>Help Requested:</span> {item.helpRequested}</p>
-                                    <p className={"ml-4 mb-11 "}><span
-                                        className={"font-bold"}>Status:</span> {item.status}</p>
-                                    <p className="ml-4"><span
-                                        className={"font-bold"}>Income Verified:</span> {item.houseHoldIncome.isIncomeVerified ? 'Yes' : 'No'}
-                                    </p>
-                                    <p className="ml-4"><span
-                                        className={"font-bold"}>License Verified:</span> {item.idSource.isValidLicense ? 'Yes' : 'No'}
-                                    </p>
-                                    <p className="ml-4"><span
-                                        className={"font-bold"}>Bus Primary Transport:</span> {item.idSource.isBusPrimaryTransport ? 'Yes' : 'No'}
-                                    </p>
+                                <div className="flex flex-col items-start">
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p><span className={"font-bold"}>Rent {'>'}1 Month Behind:</span> {item.isMoreThanMonthBehind ? 'Yes' : 'No'}</p>
+
+                                    </div>
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p><span className={"font-bold"}>Address Verified: </span>{item.homeAddress.verified ? 'Yes' : 'No'}</p>
+                                    </div>
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p><span className={"font-bold"}>Income Verified:</span> {item.houseHoldIncome.isIncomeVerified ? 'Yes' : 'No'}</p>
+                                    </div>
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p><span className={"font-bold"}>License Verified:</span> {item.idSource.isValidLicense ? 'Yes' : 'No'}</p>
+                                    </div>
+                                    <div className="whitespace-nowrap flex-1">
+                                        <p><span className={"font-bold"}>Bus Primary Transport:</span> {item.isBusPrimaryTransport ? 'Yes' : 'No'}</p>
+                                    </div>
                                 </div>
                             </div>
+                            <NeedSummaryComponent focusedItem={item} firstItem={firstItem} />
                             <div>
-                                <button onClick={toggleShowForm} className={`${styles.addButton} mt-4 mb-8`} type="button">{showForm ? `HIDE FORM` : `SHOW FORM`}</button>
-                                {showForm && <EditApplicationComponent item={item} onUpdate={updateItem} />}
+                                <div>
+                                    {showForm && <EditApplicationComponent item={item} onUpdate={updateItem} updateApplicant={updateApplicant} />}
+                                    { showForm &&
+                                        (
+                                            <div className="flex justify-end space-x-4 mb-4 mt-4">
+                                                <button onClick={toggleShowForm}
+                                                        className={`bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded`}
+                                                        type="button">{showForm ? `HIDE FORM` : `SHOW FORM`}</button>
+                                            </div>
+                                        )
+                                    }
+                                </div>
                             </div>
                         </div>
                     )}
