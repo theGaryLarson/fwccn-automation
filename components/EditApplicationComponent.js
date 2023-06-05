80a9013aaae0f985c367fb8604ba3a3b37f10357
theGaryLarson <gary.larson316@gmail.com>
2023-06-04 23:55:44 -0700

    STATE MGMT CHANGE: Moved to accordion and passed through to MemorizedApplicantForm

--- components/EditApplicationComponent.js
+++ components/EditApplicationComponent.js
@@ -3,29 +3,10 @@ import ApplicantForm from "./applicant_form/ApplicantForm";
 
 
 export default function EditApplicationComponent(props) {
-    const {item, onUpdate} = props
+    const {item, onUpdate, updateApplicant} = props
     const MemorizedApplicantForm = memo(ApplicantForm);
 
-    const  updateApplicant = async (editedItem) => {
-        try {
-            const response = await fetch(`api/getAndUpdateOneRecord`, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(editedItem)
-            });
-            if (response.ok) {
-                return await response.json();
-            } else {
-                console.error(`Error updating item for state id#: ${editedItem.idSource?.driverLicenseOrId ?? ''} `, response.status);
-            }
 
-        } catch (e) {
-            console.error(`Error updating item for state id#: ${editedItem.idSource?.driverLicenseOrId ?? ''}\n `, e);
-            return null;
-        }
-    }
 
     return (
         <div className={'bg-amber-500 m-8 p-8 rounded border-2 border-indigo-700'}>
