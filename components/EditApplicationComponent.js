2fe69dd26a9e38b4c839b782326d3679f1fa031b
theGaryLarson <gary.larson316@gmail.com>
2023-06-08 00:38:36 -0700

    STYLE: cleaned up styling

--- components/EditApplicationComponent.js
+++ components/EditApplicationComponent.js
@@ -7,8 +7,8 @@ export default function EditApplicationComponent(props) {
 
     return (
         <div className={'flex flex-col items-left w-full bg-amber-500 m-2 ml-0 p-2 rounded border-2 border-indigo-700 ' }>
-            {/*<div>Item _id {item._id}</div>*/}
             <MemorizedApplicantForm item={ item } updateApplicant={updateApplicant} onUpdate={onUpdate}/> {/* item={ item }  setEditedItem={setEditedItem}*/}
+            <div className={'text-right mt-4'}><span className={'font-bold'}>Record ObjectId:</span> {item._id}</div>
         </div>
     )
 }
