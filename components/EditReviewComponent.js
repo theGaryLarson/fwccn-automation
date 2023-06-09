dd25f6cd67c66fe00e5076a5d8c8e6879c128239
theGaryLarson <gary.larson316@gmail.com>
2023-06-09 00:24:10 -0700

    Removed unused code

--- components/EditReviewComponent.js
+++ components/EditReviewComponent.js
@@ -6,47 +6,6 @@ export default function EditReviewComponent(props) {
     // const {records, setRecords} = props
     const [queryObject, setQueryObject] = useState("")
 
-    // todo: Add more search features. This commented code is functional but isn't connected to the app in anyway
-    // const [recordCount, setRecordCount] = useState(0);
-    // useEffect(() => {
-    //     setRecordCount(records.length)
-    //     console.log(`Updated records. Count: ${records.length} records.`);
-    // }, [records, recordCount]);
-    //
-    // async function getApplicantRecords(isAddress, queryObject) {
-        // let resultRecords = isAddress ?
-        //     await getApplicantByAddress(queryObject) :
-        //     await getApplicantByStateId(queryObject);
-        // setRecords(resultRecords);
-        // console.log(resultRecords);
-    // }
-    //
-    // async function getApplicantByStateId(queryObject) {
-    //     try {
-    //         const response = await fetch(`/api/getByStateId?${queryObject}`);
-    //         if (response.ok) {
-    //             return await response.json()
-    //         } else {
-    //             console.error("Error retrieving documents by state id: ", response.status);
-    //         }
-    //     } catch (e) {
-    //         console.error("Error retrieving documents by state id: ", e);
-    //         return null;
-    //     }
-    // }
-    // async function getApplicantByAddress(queryObject) {
-    //     try {
-    //         const response = await fetch(`/api/getByAddress?${queryObject}`);
-    //         if (response.ok) {
-    //             return await response.json()
-    //         } else {
-    //             console.error("Error retrieving documents by address: ", response.status);
-    //         }
-    //     } catch (e) {
-    //         console.error("Error retrieving documents by address: ", e);
-    //         return null;
-    //     }
-    // }
  return (
      <div>
          <div className={"styles.componentWrapper"}>
