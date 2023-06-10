ada74c634dc0bcd91bd621336a949f571dd0a80d
theGaryLarson <gary.larson316@gmail.com>
2023-06-10 16:02:36 -0700

    used null chain operator

--- components/AssistanceNeedComponent.js
+++ components/AssistanceNeedComponent.js
@@ -38,7 +38,7 @@ export default function  AssistanceNeedComponent({formData, onComponentInputChan
                     <select
                         id="isMoreThanMonthBehind"
                         name="isMoreThanMonthBehind"
-                        value={formData?.homeAddress?.isMoreThanMonthBehind?.toString()??'false'}
+                        value={formData?.homeAddress?.isMoreThanMonthBehind?.toString()}
                         onChange={handleInputChange}
                     >
                         <option value={'false'}>No</option>
