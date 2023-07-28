0e28c7c9104103e4cca4e8c4b39d9d3c78f7c155
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-07-27 21:47:22 -0700

    feat: added multiple search options

--- components/RequirementCheckComponent.js
+++ components/RequirementCheckComponent.js
@@ -4,7 +4,7 @@ import Legend from "./Legend";
 
 export default function RequirementCheckComponent(props) {
     const {  queryObject  } = props;
-    const [data, setData] = useState({});
+    const [responseData, setResponseData] = useState({});
     const [firstItem, setFirstItem] = useState(undefined)
     const [ focusedItem, setFocusedItem ] = useState({})
 
@@ -18,48 +18,29 @@ export default function RequirementCheckComponent(props) {
                     },
                     body: JSON.stringify(queryObject),
                 });
-                console.log("QUERY OBJ: ", queryObject)
                 if (response.ok) {
                     return await response.json();
                 } else {
-                    console.error("Error retrieving requirements check data: ", response.status);
+                    console.error("Error retrieving requirements check responseData: ", response.status);
                 }
 
             } catch (e) {
-                console.error("Error retrieving requirements check data: ", e);
+                console.error("Error retrieving requirements check responseData: ", e);
                 return null;
             }
         }
         fetchRequirementsCheckData().then( r => {
-                setData(r)
+                setResponseData(r)
+            // setFirstItem(responseData[0])
         });
     }, [queryObject]);
-
-
-    if (!data) {
-        return <div>Loading...</div>;
-    }
-
-    //question: I used these to suppress warnings because it was working. Is this okay? (see next 3 comments)
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const ids = useMemo(() => data.stateIdDuplicates ? Object.values(data.stateIdDuplicates.records) : [], [data]);
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    const addresses = useMemo(() => data.addressDuplicates ? Object.values(data.addressDuplicates.records) : [], [data]);
-
-    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const records = useMemo(() =>  responseData, [responseData]);
     useEffect(() => {
-        if (ids && ids.length > 0) {
-            setFirstItem(ids[0]);
+        if (responseData.length > 0) {
+            setFirstItem(responseData[0]);
         }
-    }, [ids]);
+    }, [records]);
 
-    // eslint-disable-next-line react-hooks/rules-of-hooks
-    useEffect(() => {
-        if (addresses && addresses.length > 0) {
-            setFirstItem(addresses[0]);
-        }
-    }, [addresses]);
     return (
         <div>
             <div className={` w-full border-2 border-gray-600 p-4 box mt-4 mb-4`}>
@@ -67,27 +48,16 @@ export default function RequirementCheckComponent(props) {
             </div>
             <div className={` w-full border-2 border-gray-600 p-4 box mt-4 mb-4`}>
                 <h1 className={"flex items-center justify-center font-bold"}>
-                    {ids.length > 0 && 'Applicants Submission History'}
-                    {addresses.length > 0 && 'Address Submission History'}
-                    {queryObject && (ids.length < 1 && addresses.length < 1) && 'No Record of Applicant or Address'}
+                    {records.length > 0 && 'Submission History'}
+                    {records.length < 1 && 'No Record of Applicant or Address'}
                 </h1>
-                {ids && (
+                {/*ids && */(
                     <div className={"w-full"}>
-                        {ids?.map((item, index) => {
+                        { records.length > 0 && records.map((item, index) => {
                                 return (<Accordion setItemFocus={setFocusedItem} key={index} initialItem={item}
                                                    firstItem={firstItem}/>)
                             }
                         )}
-                    </div>
-                )}
-                {addresses && (
-                    <div className={"w-full"}>
-                        <div className={"place-items-center"}>
-                        </div>
-                        {addresses?.map((item, index) => (
-                            <Accordion setItemFocus={setFocusedItem} key={index} initialItem={item}
-                                       firstItem={firstItem}/>
-                        ))}
                     </div>
                 )}
             </div>
