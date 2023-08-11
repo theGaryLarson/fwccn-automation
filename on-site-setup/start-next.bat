5e38ee5b41500ff1a95adb57f11ec4fda700099b
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-08-10 17:49:07 -0700

    updated script

--- on-site-setup/start-next.bat
+++ on-site-setup/start-next.bat
@@ -1,8 +1,8 @@
 @echo off
 
-cd "C:\Users\garyl\repos\AD340-mobile-dev\FWCCN\fwccn-automation" @REM change to the directory on FWCCN server
+cd "C:\repos\fwccn-automation"
 @REM call add git pull origin main @REM pulls any changes I make to the github repo
- call npm ci @REM clean install of dependencies
- call npm run build @REM fresh production build
- call npm run start @REM starts the production web app server
+ call npm ci
+ call npm run build
+ call npm run start
 pause
