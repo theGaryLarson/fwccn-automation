4ae6788c050136db35e47d7b8ee7eed83212a8c3
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-08 23:54:11 -0700

    fix: modified error handling to correctly return error to frontend

--- src/pages/api/getAndUpdateOneRecord.js
+++ src/pages/api/getAndUpdateOneRecord.js
@@ -13,25 +13,30 @@ export default async function getAndUpdateOneRecord(req, res) {
 
     await connectMongo();
     const { _id } = req.body;
-    const updateData = req.body
-    console.log("_id: ", _id)
-    const condition = {
-        "_id": _id,
-    };
+    const updateData = req.body;
+    console.log("_id: ", _id);
+    const condition = { "_id": _id };
 
     try {
         const existingRecord = await Applicant.findOne(condition);
-        if (existingRecord) {
-            const updatedRecord = {
-                ...existingRecord?._doc,
-                ...updateData
-            };
-            await Applicant.updateOne(condition, updatedRecord);
-
-            res.json({ message: "Record updated successfully", record: updatedRecord });
+        if (!existingRecord) {
+            return res.status(404).json({ message: "Record not found", error: true });
         }
+
+        const updatedRecord = {
+            ...existingRecord._doc,
+            ...updateData
+        };
+        const updateResult = await Applicant.updateOne(condition, updatedRecord);
+
+        if (updateResult.nModified === 0) {
+            return res.status(400).json({ message: "No changes were made", error: true });
+        }
+
+        res.json({ message: "Record updated successfully", record: updatedRecord });
     } catch (error) {
         console.error(error);
-        res.status(500).json({ message: "An error occurred while updating the record" });
+        res.status(500).json({ message: "An error occurred while updating the record", error: true, details: error.message });
     }
 }
+
