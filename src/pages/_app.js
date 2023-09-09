342f675be3f8ba183c67374b2ccc4daca754e11e
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-08 22:13:04 -0700

    feat: added toast container with custom behavior

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
-
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+        <Component {...pageProps} />
+        <ToastContainer
+            position={'bottom-center'}
+            autoClose={5000} pauseOnFocusLoss={true}
+            closeOnClick={false}
+        />
+  </>
 }
