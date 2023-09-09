b15be75205dbbc43c71de3a9ba7c079e5d5e0ccb
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-08 22:39:09 -0700

    removed: Interviewed By requirement.

--- components/applicant_form/ApplicantForm.js
+++ components/applicant_form/ApplicantForm.js
@@ -87,7 +87,6 @@ function ApplicantForm(props) {
                         placeholder='Interviewer first and last name'
                         value={formData?.interviewer??''}
                         onChange={handleInputChange}
-                        required
                     />
                 </div>
                 <div className={`${styles.componentWrapper} border-2 border-black p-4 box m-4`}>
