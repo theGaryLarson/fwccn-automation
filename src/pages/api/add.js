dcd7f7a2b3b28651513e671e2f0c3b927ec34f53
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-10 00:00:35 -0700

    TODO: calculate median income and insert into object before uploading

--- src/pages/api/add.js
+++ src/pages/api/add.js
@@ -1,5 +1,6 @@
 import connectMongo from "../../../lib/connectMongo";
 import Applicant from "../../../models/applicant_schema";
+import { medianIncomeData } from "../../../models/medianIncomeData";
 
 /**
  *
@@ -7,6 +8,7 @@ import Applicant from "../../../models/applicant_schema";
  * @param {import('next').NextApiResponse} res
  */
 export default async function addApplicant(req, res) {
+    console.log(req.body.data.totalHouseholdIncome.incomeLevel);
     await connectMongo();
     const applicant = await Applicant.create(req.body.data);
     res.json(applicant);
