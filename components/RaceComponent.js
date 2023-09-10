2383442dc58463df8a3143bd0e36185cbf15c1aa
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-10 00:04:26 -0700

    added: ethnicities required for ARPA report

--- components/RaceComponent.js
+++ components/RaceComponent.js
@@ -10,18 +10,7 @@ export default function RaceComponent({formData, onComponentInputChange }) {
         <div>
             <h1>Demographic Information</h1>
             <div className={styles.componentWrapper}>
-                <label htmlFor="americanIndianOrAlaskaNative">American Indian or Alaska Native</label>
-                <input
-                    type="number"
-                    id="americanIndianOrAlaskaNative"
-                    name="americanIndianOrAlaskaNative"
-                    value={formData?.race?.americanIndianOrAlaskaNative??0}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className={styles.componentWrapper}>
-                <label htmlFor="whiteOrCaucasian">White or Caucasian</label>
+                <label htmlFor="whiteOrCaucasian">White/Caucasian</label>
                 <input
                     type="number"
                     id="whiteOrCaucasian"
@@ -32,12 +21,100 @@ export default function RaceComponent({formData, onComponentInputChange }) {
                 />
             </div>
             <div className={styles.componentWrapper}>
-                <label htmlFor="asianAsianAmerican">Asian or Asian American</label>
+                <label htmlFor="blackAfricanAmerican">Black/African American</label>
+                <input
+                    type="number"
+                    id="blackAfricanAmerican"
+                    name="blackAfricanAmerican"
+                    value={formData?.race?.blackAfricanAmerican??0}
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+            <div className={styles.componentWrapper}>
+                <label htmlFor="asianAsianAmerican">Asian/Asian American</label>
                 <input
                     type="number"
                     id="asianAsianAmerican"
                     name="asianAsianAmerican"
                     value={formData?.race?.asianAsianAmerican??0}
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+            <div className={styles.componentWrapper}>
+                <label htmlFor="americanIndianOrAlaskaNative">American Indian/Alaska Native</label>
+                <input
+                    type="number"
+                    id="americanIndianOrAlaskaNative"
+                    name="americanIndianOrAlaskaNative"
+                    value={formData?.race?.americanIndianOrAlaskaNative??0}
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+            <div className={styles.componentWrapper}>
+                <label htmlFor="nativeHawaiianPacificIslander">Native Hawaiian/Pacific Islander</label>
+                <input
+                    type="number"
+                    id="nativeHawaiianPacificIslander"
+                    name="nativeHawaiianPacificIslander"
+                    value={formData?.race?.nativeHawaiianPacificIslander??0}
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+            <div className={styles.componentWrapper}>
+                <label htmlFor="latinoAmericanHispanic">Latinx/Hispanic</label>
+                <input
+                    type="number"
+                    id="latinoAmericanHispanic"
+                    name="latinoAmericanHispanic"
+                    value={formData?.race?.latinoAmericanHispanic??0}
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+            <div className={styles.componentWrapper}>
+                <label htmlFor="americanIndianOrAlaskaNativeAndWhite">American Indian/Alaska Native & White</label>
+                <input
+                    type="number"
+                    id="americanIndianOrAlaskaNativeAndWhite"
+                    name="americanIndianOrAlaskaNativeAndWhite"
+                    value={formData?.race?.americanIndianOrAlaskaNativeAndWhite??0}
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+            <div className={styles.componentWrapper}>
+                <label htmlFor="asianAsianAmericanAndWhite">Asian/Asian American & White</label>
+                <input
+                    type="number"
+                    id="asianAsianAmericanAndWhite"
+                    name="asianAsianAmericanAndWhite"
+                    value={formData?.race?.asianAsianAmericanAndWhite??0}
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+            <div className={styles.componentWrapper}>
+                <label htmlFor="blackAfricanAmericanAndWhite">Black/African American & White</label>
+                <input
+                    type="number"
+                    id="blackAfricanAmericanAndWhite"
+                    name="blackAfricanAmericanAndWhite"
+                    value={formData?.race?.blackAfricanAmericanAndWhite??0}
+                    onChange={handleInputChange}
+                    required
+                />
+            </div>
+            <div className={styles.componentWrapper}>
+                <label htmlFor="americanIndianOrAlaskaNativeAndBlackAfricanAmerican">American Indian/Alaska Native & Black/African American</label>
+                <input
+                    type="number"
+                    id="americanIndianOrAlaskaNativeAndBlackAfricanAmerican"
+                    name="americanIndianOrAlaskaNativeAndBlackAfricanAmerican"
+                    value={formData?.race?.americanIndianOrAlaskaNativeAndBlackAfricanAmerican??0}
                     onChange={handleInputChange}
                     required
                 />
@@ -54,17 +131,6 @@ export default function RaceComponent({formData, onComponentInputChange }) {
                 />
             </div>
             <div className={styles.componentWrapper}>
-                <label htmlFor="blackAfricanAmerican">Black or African American</label>
-                <input
-                    type="number"
-                    id="blackAfricanAmerican"
-                    name="blackAfricanAmerican"
-                    value={formData?.race?.blackAfricanAmerican??0}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className={styles.componentWrapper}>
                 <label htmlFor="multiRacial">Multi-Racial</label>
                 <input
                     type="number"
@@ -76,34 +142,12 @@ export default function RaceComponent({formData, onComponentInputChange }) {
                 />
             </div>
             <div className={styles.componentWrapper}>
-                <label htmlFor="latinoAmericanHispanic">Latino or Hispanic</label>
-                <input
-                    type="number"
-                    id="latinoAmericanHispanic"
-                    name="latinoAmericanHispanic"
-                    value={formData?.race?.latinoAmericanHispanic??0}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className={styles.componentWrapper}>
                 <label htmlFor="unknown">Unknown</label>
                 <input
                     type="number"
                     id="unknown"
                     name="unknown"
                     value={formData?.race?.unknown??0}
-                    onChange={handleInputChange}
-                    required
-                />
-            </div>
-            <div className={styles.componentWrapper}>
-                <label htmlFor="nativeAmericanPacificIslander">Native Hawaiian or Pacific Islander</label>
-                <input
-                    type="number"
-                    id="nativeAmericanPacificIslander"
-                    name="nativeAmericanPacificIslander"
-                    value={formData?.race?.nativeAmericanPacificIslander??0}
                     onChange={handleInputChange}
                     required
                 />
