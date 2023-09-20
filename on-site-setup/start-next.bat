f802e2fd6944d758ad70d511759cea123654ac3a
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-20 15:57:30 -0700

    refactor: use laptop setup

--- on-site-setup/start-next.bat
+++ on-site-setup/start-next.bat
@@ -1,6 +1,6 @@
 @echo off
 
-cd "C:\repos\fwccn-automation"
+cd "C:\fwccn\webApp\fwccn-automation"
 @REM call add git pull origin main @REM pulls any changes I make to the github repo
  call npm ci
  call npm run build
