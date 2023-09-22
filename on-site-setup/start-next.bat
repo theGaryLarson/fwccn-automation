bea7bf77489c6aa569a2de7efe3ffa13a1d40e09
theGaryLarson <gary.larson@seattlecolleges.edu>
2023-09-22 15:43:05 -0700

    setup: finalizing script

--- on-site-setup/start-next.bat
+++ on-site-setup/start-next.bat
@@ -1,8 +1,19 @@
 @echo off
 
 cd "C:\fwccn\webApp\fwccn-automation"
-@REM call add git pull origin main @REM pulls any changes I make to the github repo
- call npm ci
- call npm run build
- call npm run start
-pause
+@REM Check if the current branch is 'main'
+for /f %%i in ('git rev-parse --abbrev-ref HEAD') do set branch=%%i
+if "%branch%" neq "main" (
+    echo You are not on the 'main' branch. Switching to 'main'...
+    git checkout main
+)
+
+@REM Pull the latest changes from 'origin' and hard reset
+echo Pulling latest changes from 'origin/main'...
+git fetch origin main
+git reset --hard origin/main
+
+@REM Run npm commands
+call npm ci
+call npm run build
+call npm run start
