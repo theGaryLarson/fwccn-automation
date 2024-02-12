f1eaea61a6d37c6318a24b4d6c068f3a7d23510b
theGaryLarson <gary.larson@seattlecolleges.edu>
2024-02-11 16:19:40 -0800

    Update input type and placeholder in DemographicComponent
    Changed the input types from "number" to "text" for household member count fields in the DemographicComponent. Also, added placeholders to guide the user and enhance data entry usability.

--- components/DemographicComponent.js
+++ components/DemographicComponent.js
@@ -14,23 +14,25 @@ export default function DemographicComponent({formData, onComponentInputChange }
             <div className={'flex h-10 justify-between mb-4'}>
                 <div className='flex flex-col'><label className={'font-bold'} htmlFor="totalMales">Total Males In Household:</label>
                     <input
-                        type="number"
+                        type="text"
                         id="totalMales"
                         name="totalMales"
                         value={formData?.demographics?.totalMales ?? 0}
                         onChange={handleInputChange}
                         className={'pl-1'}
-                        required
+                        placeholder='Enter total males'
+
                     /></div>
                 <div className='flex flex-col '><label className={'font-bold'} htmlFor="totalFemales">Total Females In Household:</label>
                     <input
-                        type="number"
+                        type="text"
                         id="totalFemales"
                         name="totalFemales"
                         value={formData?.demographics?.totalFemales ?? 0}
                         onChange={handleInputChange}
                         className={'pl-1'}
-                        required
+                        placeholder='Enter total females'
+
                     />
                 </div>
             </div>
@@ -56,157 +58,170 @@ export default function DemographicComponent({formData, onComponentInputChange }
             <div className={styles.componentWrapper}>
                 <label htmlFor="whiteOrCaucasian">White/Caucasian:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="whiteOrCaucasian"
                     name="whiteOrCaucasian"
                     value={formData?.demographics?.whiteOrCaucasian??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="blackAfricanAmerican">Black/African American:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="blackAfricanAmerican"
                     name="blackAfricanAmerican"
                     value={formData?.demographics?.blackAfricanAmerican??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="asianAsianAmerican">Asian/Asian American:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="asianAsianAmerican"
                     name="asianAsianAmerican"
                     value={formData?.demographics?.asianAsianAmerican??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="americanIndianOrAlaskaNative">American Indian/Alaska Native:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="americanIndianOrAlaskaNative"
                     name="americanIndianOrAlaskaNative"
                     value={formData?.demographics?.americanIndianOrAlaskaNative??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="nativeHawaiianPacificIslander">Native Hawaiian/Pacific Islander:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="nativeHawaiianPacificIslander"
                     name="nativeHawaiianPacificIslander"
                     value={formData?.demographics?.nativeHawaiianPacificIslander??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="latinoAmericanHispanic">Latinx/Hispanic:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="latinoAmericanHispanic"
                     name="latinoAmericanHispanic"
                     value={formData?.demographics?.latinoAmericanHispanic??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="americanIndianOrAlaskaNativeAndWhite">American Indian/Alaska Native & White:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="americanIndianOrAlaskaNativeAndWhite"
                     name="americanIndianOrAlaskaNativeAndWhite"
                     value={formData?.demographics?.americanIndianOrAlaskaNativeAndWhite??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="asianAsianAmericanAndWhite">Asian/Asian American & White:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="asianAsianAmericanAndWhite"
                     name="asianAsianAmericanAndWhite"
                     value={formData?.demographics?.asianAsianAmericanAndWhite??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="blackAfricanAmericanAndWhite">Black/African American & White:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="blackAfricanAmericanAndWhite"
                     name="blackAfricanAmericanAndWhite"
                     value={formData?.demographics?.blackAfricanAmericanAndWhite??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="americanIndianOrAlaskaNativeAndBlackAfricanAmerican">American Indian/Alaska Native & Black/African American:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="americanIndianOrAlaskaNativeAndBlackAfricanAmerican"
                     name="americanIndianOrAlaskaNativeAndBlackAfricanAmerican"
                     value={formData?.demographics?.americanIndianOrAlaskaNativeAndBlackAfricanAmerican??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="otherRace">Other Race:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="otherRace"
                     name="otherRace"
                     value={formData?.demographics?.otherRace??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="multiRacial">Multi-Racial:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="multiRacial"
                     name="multiRacial"
                     value={formData?.demographics?.multiRacial??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in this ethnic group.'
+
                 />
             </div>
             <div className={styles.componentWrapper}>
                 <label htmlFor="unknown">Unknown:</label>
                 <input
-                    type="number"
+                    type="text"
                     id="unknown"
                     name="unknown"
                     value={formData?.demographics?.unknown??0}
                     onChange={handleInputChange}
                     className={'pl-1'}
-                    required
+                    placeholder='Enter count of total household members in an unknown ethnic group.'
+
                 />
             </div>
         </div>
